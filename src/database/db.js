ab3be3401f82bb0489f6552b1a04ee2ab90af538
Adeildo Neto <n.santos1897@gmail.com>
2023-09-26 19:17:34 -0300

    Add database ORM

---
+++ src/database/db.js
@@ -0,0 +1,12 @@
+const { Sequelize } = require('sequelize');
+
+// TODO: Store these properties in a config file.
+const database = new Sequelize({
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'postgres',
+    host: '127.0.0.1',
+    port: '5432'
+  });
+
+module.exports = database;
